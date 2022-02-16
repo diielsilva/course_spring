3541cc543803fc65b30f738511dfe23237e11efa
Daniel Silva de Sousa <diielsilvasousa@gmail.com>
2022-02-16 20:13:18 -0300

    Adicionando banco de dados H2 e utilizacao do jpa para criacao das tabelas

--- src/main/java/com/dev/course_spring/domains/Category.java
+++ src/main/java/com/dev/course_spring/domains/Category.java
@@ -3,8 +3,16 @@ package com.dev.course_spring.domains;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 
