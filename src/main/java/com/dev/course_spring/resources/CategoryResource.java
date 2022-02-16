7d351619b4c29793f6ff332b2b882f86c3e3cf0b
Daniel Silva de Sousa <diielsilvasousa@gmail.com>
2022-02-16 19:51:02 -0300

    Adicionando a classe de dominio Categoria

--- src/main/java/com/dev/course_spring/resources/CategoryResource.java
+++ src/main/java/com/dev/course_spring/resources/CategoryResource.java
@@ -1,15 +1,25 @@
 package com.dev.course_spring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.course_spring.domains.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String test() {
-		return "Rest Testado";
+	public List<Category> test() {
+		Category categroy01 = new Category(1, "Informática");
+		Category category02 = new Category(2, "Escritório");
+		List<Category> categories = new ArrayList<Category>();
+		categories.add(categroy01);
+		categories.add(category02);
+		return categories;
 	}
 }
